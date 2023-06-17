dee1370b58c02520dd5b41cea9a861510f9a5380
Laura <laura.glassjohnston@gmail.com>
2023-06-17 15:15:25 -0400

    pre-presentation commit

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,7 @@ const db = require('./dbModel');
 controller.getMatches = async (req, res, next) => {
   try {
     const id = 1;
-    const getMatches = `SELECT * FROM Matches WHERE login_user = ${id}`;
+    const getMatches = 'SELECT p.id, p.name, p.owner, p.zip, p.breed, p.size, p.age, p.gender FROM pooches p RIGHT OUTER JOIN matches ON matches.matched_user = p.id WHERE matches.login_user = 1';
     const listOfMatches = await db.query(getMatches);
     console.log(listOfMatches);
     res.locals.matches = listOfMatches.rows;
