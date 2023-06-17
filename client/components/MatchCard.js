5a3a3041717a261cbf6792874c4443c6b6f31ad1
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-06-17 00:51:12 -0400

    Added react-router and skeleton components

---
+++ client/components/MatchCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export default function MatchCard() {
+    // send calendar invite function
+
+    // remove match function
+    return (
+        <div>
+            <img />
+            {/* Send Calendar Invite */}
+            <button></button>
+            {/* Remove Match */}
+            <button></button>
+        </div>
+    )
+};
