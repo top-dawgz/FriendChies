a682887e729577800c855b8eca837d5c8aba90d8
Alex Vranas <avranas42@gmail.com>
2023-06-20 16:54:44 -0400

    Add Swipe Page

--- client/components/Card.js
+++ client/components/Card.js
@@ -1,80 +1,81 @@
 import React from "react";
 export default function Card(props) {
-  const { dogInf } = props;
+  const { dog } = props;
 
   // like function
   function handleLike() {
-    const data = { id: id, userId: userId };
-    //send a post request to back end
-    //fetch ('someURL', method:Post)
-    fetch("URL", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: json.stringify(data),
-    })
-      .then((data) => {
-        data.json();
-      })
-      .then((response) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        next({ err: { message: "match request unsuccessful " } });
-      });
-    //Invoke the parent function to update state of current dog
-    handleChangeDog();
+    // const data = { id: id, userId: userId };
+    // //send a post request to back end
+    // //fetch ('someURL', method:Post)
+    // fetch("URL", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: json.stringify(data),
+    // })
+    //   .then((data) => {
+    //     data.json();
+    //   })
+    //   .then((response) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     next({ err: { message: "match request unsuccessful " } });
+    //   });
+    // //Invoke the parent function to update state of current dog
+    props.goToNextDog();
   }
 
   // dislike function
   function handleDislike() {
     //invoke the parent function to update
-    handleChangeDog();
+    //handleChangeDog();
+    props.goToNextDog();
   }
 
   return (
     <div className="card">
-      <h3>{dogInf.name}</h3>
+      <h3>{dog.name}</h3>
       {/* Carousel to swipe through pictures? */}
       {/* <img className="swipeCardIm" src={link} /> */}
       <ul class="removeBullets">
         <li>
           <label class="cardLabel" id="owner">
-            <strong>Owner Name:</strong>
-            {dogInf.owner}
+            <strong>Owner Name: </strong>
+            {dog.owner}
           </label>
         </li>
 
         <li>
           <label class="cardLabel" id="breed">
-            <strong>Breed:</strong>
-            {dogInf.breed}
+            <strong>Breed: </strong>
+            {dog.breed}
           </label>
         </li>
 
         <li>
           <label class="cardLabel" id="size">
-            <strong>Size:</strong>
-            {dogInf.size}
+            <strong>Size: </strong>
+            {dog.size}
           </label>
         </li>
 
         <li>
           <label class="cardLabel" id="breed">
-            <strong>Gender:</strong>
-            {dogInf.gender}
+            <strong>Gender: </strong>
+            {dog.sex}
           </label>
         </li>
 
         <li>
           <label class="cardLabel" id="breed">
-            <strong>age:</strong>
-            {dogInf.age}
+            <strong>age: </strong>
+            {dog.age}
           </label>
         </li>
       </ul>
 
       {/* Like Button */}
-      <button class="buttonCard" id="dislike" onClick={handledisLike}>
+      <button class="buttonCard" id="dislike" onClick={handleDislike}>
         No Paw
       </button>
       {/* Dislike Button */}
--- client/pages/SwipePage.js
+++ client/pages/SwipePage.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from "react";
-
-import Card from "../components/Card.js";
+import React, { useState, useEffect } from 'react';
+import mockData from './mock-data.js';
+import Card from '../components/Card.js';
 
 export default function SwipePage() {
-  const [dog, setDog] = useState();
-  const [i, setI] = useState(0);
+  const [dogList, setDogList] = useState();
+  const [listIndex, setListIndex] = useState(0);
+  const [currentDog, setCurrentDog] = useState();
 
   useEffect(() => {
-    fetch("URL", {
-      method: "GET",
-    })
-      .then((data) => {
-        data.json();
-      })
-      .then(setDog(data));
+    async function getDogs() {
+      //const response = await axios(''); //Get potential likes
+      const response = mockData;
+      console.log('response', response);
+      setDogList(response);
+      setCurrentDog(response[0]);
+    }
+    getDogs();
   }, []);
 
-  function handleChangeDog() {
-    setI(i + 1);
+  function goToNextDog() {
+    setListIndex(listIndex + 1);
+    setCurrentDog(dogList[listIndex + 1]);
   }
-
+  console.log('currentDog', currentDog)
   return (
     <div>
       <h3>Swipe Page</h3>
-      <Card dogInf={dog[i]} />
+      {currentDog ? <Card goToNextDog={goToNextDog} dog={currentDog} /> : null}
+      <button onClick={goToNextDog}>Next</button>
     </div>
   );
 }
