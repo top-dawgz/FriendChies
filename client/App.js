a71e0f7e13987bb9a1b00a19b34462b03f143abe
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-06-20 09:52:18 -0400

    Remove build file and incomplete route pages for clean working dev

--- client/App.js
+++ client/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-import MatchPage from "./pages/MatchPage";
-import SwipePage from "./pages/SwipePage";
-
 export default function App() {
   return (
     <div>
       <h1>FriendChies</h1>
-      <SwipePage />
     </div>
   );
 }
