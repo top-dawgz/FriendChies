b236b7fc280caac978c6ca0b62a903fcb3042023
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-06-21 10:35:30 -0400

    Divide MatchPage into 3 sections. Complete MatchList.

--- server/controllers/dogController.js
+++ server/controllers/dogController.js
@@ -18,11 +18,11 @@ dogController.getMatches = async (req, res, next) => {
     // id should be profile id of logged in user
     const id = 1;
     const getMatches = `
-      SELECT name, owner
+      SELECT name, owner, match_id, img_src
       FROM dogProfiles dp
       JOIN matches 
-      ON matches.match_id = dp.profile_id 
-      WHERE matches.user_id = $1
+      ON matches.match_id = dp.id 
+      WHERE matches.profile_id = $1
     `;
     const listOfMatches = await db.query(getMatches, [id]);
     res.locals.matches = listOfMatches.rows;
@@ -37,10 +37,10 @@ dogController.getProfile = async (req, res, next) => {
     const profileId = req.params.profileId;
     const getProfile = `
       SELECT * FROM dogProfiles dp
-      WHERE dp.profile_id = $1
+      WHERE dp.id = $1
     `;
     const profile = await db.query(getProfile, [profileId]);
-    res.locals.profile = profile.rows;
+    res.locals.profile = profile.rows[0];
 
     return next();
   } catch (err){
